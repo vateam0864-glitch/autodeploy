d47da2b1e7f9405cbb8ec0e21674395f7e970ff7
Dani <vateam0864@gmail.com>
2025-12-09 17:31:22 +0800

    test print fix

--- app1/app.py
+++ app1/app.py
@@ -3,11 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from app1 hh!"
+    return "Hello from app1!"
 
-    print("hello")
-    print("hey how are you ")
+print("hello")  # This will show when container starts
+print("hey how are you")  # This will show when container starts
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
-
