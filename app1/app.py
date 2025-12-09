c67d74ea8e30279e96b54a0b09d6f6b974d53b5b
Dani <vateam0864@gmail.com>
2025-12-09 17:29:43 +0800

    test print fix

--- app1/app.py
+++ app1/app.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from app1!"
+    return "Hello from app1 hh!"
+
+    print("hello")
+    print("hey how are you ")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
 
-
-    print("hello")
-    print("hey how are you ")
