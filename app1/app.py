c1d88849848a93beb3da9ac61f34bfc7598e585f
Dani <vateam0864@gmail.com>
2025-12-10 00:10:13 +0800

    display date&time

--- app1/app.py
+++ app1/app.py
@@ -1,17 +1,18 @@
 from flask import Flask
+from datetime import datetime
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello from app1!"
+@app.route("/")
+def show_datetime():
+    now = datetime.now()
+    return f"""
+    <meta http-equiv="refresh" content="1">
+    <h1>⏱ Live Date & Time</h1>
+    <p><b>Date:</b> {now.strftime("%Y-%m-%d")}</p>
+    <p><b>Time:</b> {now.strftime("%H:%M:%S")}</p>
+    <p><b>Year:</b> {now.strftime("%Y")}</p>
+    """
 
-print("hello")  # This will show when container starts
-print("hey how are you")  # This will show when container starts
-print("this is app1")
-print("this is app1 with func")
-print("NEW UPDATE SUCCESSFUL!!!")
-print("new 1 ")
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
